f36fb5ee31c829677c73c73b626cce7d05b5eea4
patgwillia@gmail.com <patgwillia@gmail.com>
2024-09-23 11:06:33 -0400

    updating NYTTester.py

--- ansible/roles/patsite/files/wordChecker/NYTTester.py
+++ ansible/roles/patsite/files/wordChecker/NYTTester.py
@@ -7,20 +7,23 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import \
-    ElementNotInteractableException as noninteractable
+from selenium.common.exceptions import (
+    ElementNotInteractableException as noninteractable,
+)
 from selenium.common.exceptions import NoSuchElementException as noElement
 from selenium.common.exceptions import TimeoutException as timeOut
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 
 import checkDictionary
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='/home/ansible/app_files/wordChecker/NYTTester.log', level=logging.INFO)
+logging.basicConfig(
+    filename="/home/pat/projects/patrick-williams/seleniumTestingDir/NYTTester.log", level=logging.INFO
+)
+
 
 class MyWebDriver:
 
@@ -30,29 +33,10 @@ class MyWebDriver:
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--headless=new")
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.set_page_load_timeout(60)
+        self.driver.set_page_load_timeout(120)
         self.driver.set_script_timeout(120)
         self.hiveLetters = []
-        self.driver.get("https://www.nytimes.com/puzzles/spelling-bee")
-
-        startButton = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(
-                (
-                    By.XPATH,
-                    "/html/body/div[3]/div[2]/div[2]/div[2]/section[2]/div/div/div/div[2]/div/button[1]",
-                )
-            )
-        )
-        startButton.click()
-        self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
-        hiveElement = self.soup.find(class_="hive")
-        self.enterButton = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, "div.hive-action:nth-child(1)")
-            )
-        )
-        for letter in hiveElement.contents:
-            self.hiveLetters.append(letter.text)
+        self.soup = []
 
     def clickLetters(self, state, word):
         flag = True
@@ -83,23 +67,47 @@ class MyWebDriver:
             if wordListContents.text not in state.acceptedWords:
                 state.acceptedWords.append(wordListContents.text)
 
+    def getInitialValues(self):
+
+        startButton = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "/html/body/div[3]/div[2]/div[2]/div[2]/section[2]/div/div/div/div[2]/div/button[1]",
+                )
+            )
+        )
+        startButton.click()
+        self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        hiveElement = self.soup.find(class_="hive")
+        self.enterButton = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "div.hive-action:nth-child(1)")
+            )
+        )
+        for letter in hiveElement.contents:
+            self.hiveLetters.append(letter.text)
+
 
 class State:
 
     def __init__(self):
         self.answer = []
         self.acceptedWords = []
-        self.rejectedWords= []
+        self.rejectedWords = []
         self.sortedLetters = []
+        self.attempts = 0
+        self.web = []
+        self.initializeDriver()
 
-    def checkWords(self, web):
+    def checkWords(self):
         for word in self.answer:
             logger.info("checking ")
             logger.info(word)
             atLimit = False
 
             try:
-                web.clickLetters(self, word)
+                self.web.clickLetters(self, word)
                 time.sleep(1)
             except noninteractable:
 
@@ -108,40 +116,77 @@ class State:
                 atLimit = True
 
             if atLimit:
-                logger.info("closing driver")
-                try:
-                    web.driver.close()
-                    logger.info("dirver successfully closed")
-                except timeOut:
-                    logger.info("Timeout Exception Occured")
-                    web.driver.quit()
-                    logger.info("dirver quit")
-                web = MyWebDriver()
+                logger.info("At limit")
+                self.closeDriver()
+                self.initializeDriver()
                 time.sleep(2)
-                web.clickLetters(self, word)
+                self.web.clickLetters(self, word)
                 time.sleep(1)
-        self.sortLetters(web.hiveLetters)
-        web.driver.quit()
+        self.sortLetters()
+        self.closeDriver()
 
-    def sortLetters(self, letters):
+    def closeDriver(self):
+            logger.info("closing driver")
+            try:
+                self.web.driver.close()
+                logger.info("dirver successfully closed")
+            except timeOut:
+                logger.info("Timeout Exception Occured")
+                self.web.driver.quit()
+                logger.info("dirver quit")
+
+
+    def sortLetters(self):
+        letters = self.web.hiveLetters
         required = letters.pop(0)
         letters.sort()
         letters.insert(0, required)
         stringLetters = "".join(letters)
         self.sortedLetters = stringLetters
 
+    def getWebsite(self):
+        getAttempts = 0
+        for getAttempts in range(5):
+            try:
+                logger.info("getting nyt.com")
+                self.web.driver.get("https://www.nytimes.com/puzzles/spelling-bee")
+                getAttempts = 0
+                break
+            except timeOut:
+                logger.info("nyt.com timed out. attempts: ", getAttempts + 1)
+                raise timeOut
+        if getAttempts >= 4:
+            raise timeOut("failed to load page") 
+
+    def initializeDriver(self):
+        fullAttempts = 0
+
+        for fullAttempts in range(5):
+            self.web = MyWebDriver()
+            try:
+                self.getWebsite()
+                fullAttempts=0
+                break
+            except timeOut:
+                self.closeDriver()
+
+        if fullAttempts >=4:
+            raise Exception("failed to load spelling bee page many times")
+
+        self.web.getInitialValues()
+
+
 
 
 def findAcceptedWords():
-    web = MyWebDriver()
     state = State()
-    checker = checkDictionary.dictionaryChecker(web.hiveLetters)
+    checker = checkDictionary.dictionaryChecker(state.web.hiveLetters)
     if checker.answer is not None:
         state.answer = checker.answer
         logger.info("found words: ")
         logger.info(state.answer)
-        state.checkWords(web)
-    checker.findPangrams(state.sortedLetters,state.acceptedWords)
+        state.checkWords()
+    checker.findPangrams(state.sortedLetters, state.acceptedWords)
     if checker.pangrams is not None:
         for word in checker.pangrams:
             state.acceptedWords.remove(word)
@@ -153,20 +198,21 @@ def findAcceptedWords():
     logger.info(checker.pangrams)
     logger.info("letters: ")
     logger.info(state.sortedLetters)
-            
+
     db = sqlite3.connect(
-            "/home/ansible/app_files/shared_files/spellingBeeDB.sqlite",
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-    db.execute(
-        "DELETE FROM bees"
-        " WHERE letters=?",
-        (state.sortedLetters,)
+        "/home/ansible/app_files/shared_files/spellingBeeDB.sqlite",
+        detect_types=sqlite3.PARSE_DECLTYPES,
     )
+    db.execute("DELETE FROM bees" " WHERE letters=?", (state.sortedLetters,))
     db.execute(
         "INSERT INTO bees (letters, accepted, pangrams, rejected)"
         " VALUES (?, ?, ?, ?)",
-        (state.sortedLetters, json.dumps(state.acceptedWords), json.dumps(checker.pangrams), json.dumps(state.rejectedWords))
+        (
+            state.sortedLetters,
+            json.dumps(state.acceptedWords),
+            json.dumps(checker.pangrams),
+            json.dumps(state.rejectedWords),
+        ),
     )
     db.commit()
     db.close()
