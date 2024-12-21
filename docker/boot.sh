0108856d013fdfae30b1411ef79b77560d5cb0d5
patgwillia@gmail.com <patgwillia@gmail.com>
2024-12-21 10:45:00 -0500

    clean up

--- docker/boot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 -m flask --app PatrickWilliamsWebsite db migrate
-python3 -m flask --app PatrickWilliamsWebsite db upgrade
-exec python3 -m waitress --port=8041 --call PatrickWilliamsWebsite:create_app
